197a70d0f4e4e1a82a8d6343760c604481ebeec7
dannychl <dannyluchunhung@gmail.com>
2022-02-15 14:43:16 +0800

    First Upload
    CRUD created

---
+++ RecruitmentManagementSystem (Danny)/Controllers/CandidatesController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RecruitmentManagementSystem__Danny_.DAL;
+using RecruitmentManagementSystem__Danny_.Models;
+
+namespace RecruitmentManagementSystem__Danny_.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+        Home Candidate = new Home();
+
+        // GET: Candidates
+        public ActionResult Index()
+        {
+            /*Home home = new Home()
+            {
+                DateOfBirth = DateTime.Now
+            };*/
+            //return View(home);
+            //return View(db.DateOfBirth.Add(home));
+
+            return View(db.Candidate.ToList());
+        }
+
+        // GET: Candidates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Home home = db.Candidate.Find(id);
+            if (home == null)
+            {
+                return HttpNotFound();
+            }
+            return View(home);
+        }
+
+        // GET: Candidates/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Age, Position, ExpectedSalary, DateOfBirth, Gender")] Home home)
+        {
+            if (ModelState.IsValid)
+            {
+             
+                int currentAge = home.DateOfBirth.Year;
+                int curAge = DateTime.Now.Year;
+
+                int exactAge = curAge - currentAge;
+
+                home.Age = exactAge;
+                db.Candidate.Add(home);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(home);
+        }
+
+        // GET: Candidates/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Home home = db.Candidate.Find(id);
+            if (home == null)
+            {
+                return HttpNotFound();
+            }
+            return View(home);
+        }
+
+        // POST: Candidates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Age,Position,ExpectedSalary, DateOfBirth, Gender")] Home home)
+        {
+            if (ModelState.IsValid)
+            {
+                int currentAge = home.DateOfBirth.Year;
+                int curAge = DateTime.Now.Year;
+
+                int exactAge = curAge - currentAge;
+
+                home.Age = exactAge;
+                db.Entry(home).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(home);
+        }
+
+        // GET: Candidates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Home home = db.Candidate.Find(id);
+            if (home == null)
+            {
+                return HttpNotFound();
+            }
+            return View(home);
+        }
+
+        // POST: Candidates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Home home = db.Candidate.Find(id);
+            db.Candidate.Remove(home);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RecruitmentManagementSystem (Danny)/DAL/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using RecruitmentManagementSystem__Danny_.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace RecruitmentManagementSystem__Danny_.DAL
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext() : base("DatabaseContext")
+        {
+        }
+
+        public DbSet<Home> Candidate { get; set; }
+
+    }
+
+
+}
