f0886accb3f7e03033b3c69f360129ddc0e72b32
SkyFoo012 <skyfoosk@gmail.com>
2022-02-18 17:12:55 +0800

    InterviewManagementV2
    Changed Interview page
    Added change candidates status to "Accept" -> Insert into Interviewee database table

--- RecruitmentManagementSystem (Danny)/Controllers/InterviewController.cs
+++ RecruitmentManagementSystem (Danny)/Controllers/InterviewController.cs
@@ -17,13 +17,9 @@ namespace RecruitmentManagementSystem__Danny_.Controllers
         //Home home = new Home();
         // GET: Interview
         public ActionResult Index()
-        {
-            var model = from s in db.Candidate
-            where s.Status == "Accept"
-            select s;
-                       
-            return View(model);
-         }
+        {         
+            return View(db.Interviewer.ToList());
+        }
 
         // GET: Interview/Details/5
         public ActionResult Details(int? id)
@@ -85,25 +81,29 @@ namespace RecruitmentManagementSystem__Danny_.Controllers
         }
 
         // GET: Interview/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Interview Interviewer = db.Interviewer.Find(id);
+            if (Interviewer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Interviewer);
         }
 
         // POST: Interview/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            Interview Interviewer = db.Interviewer.Find(id);
+            db.Interviewer.Remove(Interviewer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
---
+++ RecruitmentManagementSystem (Danny)/Controllers/ShowAllCandidatesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RecruitmentManagementSystem__Danny_.DAL;
+using RecruitmentManagementSystem__Danny_.Models;
+
+namespace RecruitmentManagementSystem__Danny_.Controllers
+{
+    public class ShowAllCandidatesController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+
+        // GET: ShowAllCandidates
+        public ActionResult Index()
+        {
+            return View(db.Candidate.ToList());
+        }
+
+        // GET: ShowAllCandidates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Home home = db.Candidate.Find(id);
+            if (home == null)
+            {
+                return HttpNotFound();
+            }
+            return View(home);
+        }
+
+        // GET: ShowAllCandidates/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShowAllCandidates/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,MethodUsed,PhoneNum,Gender,Age,DateOfBirth,Position,ExpectedSalary,CurrentSalary,WorkingExperience,WorkingExperienceRemarks,ResignPeriod,ProgrammingTest,SQLTest,TestRemarks,Status")] Home home)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Candidate.Add(home);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(home);
+        }
+
+        // GET: ShowAllCandidates/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Home home = db.Candidate.Find(id);
+            if (home == null)
+            {
+                return HttpNotFound();
+            }
+            return View(home);
+        }
+
+        // POST: ShowAllCandidates/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,MethodUsed,PhoneNum,Gender,Age,DateOfBirth,Position,ExpectedSalary,CurrentSalary,WorkingExperience,WorkingExperienceRemarks,ResignPeriod,ProgrammingTest,SQLTest,TestRemarks,Status")] Home home)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(home).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(home);
+        }
+
+        // GET: ShowAllCandidates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Home home = db.Candidate.Find(id);
+            if (home == null)
+            {
+                return HttpNotFound();
+            }
+            return View(home);
+        }
+
+        // POST: ShowAllCandidates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Home home = db.Candidate.Find(id);
+            db.Candidate.Remove(home);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
